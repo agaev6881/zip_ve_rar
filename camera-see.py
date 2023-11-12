918f71378eacad5e2625e7adcd9c2492460bd43b
Ülvi Ağayev <82797278+agaev6881@users.noreply.github.com>
2023-11-12 19:01:50 +0400

    Add files via upload

---
+++ camera-see.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+import os
+import time
+import platform
+import random
+import datetime
+#Mor = '\033[95m'
+#Cyan = '\033[96m'
+#KoyuMavi = '\033[1;34m'
+#Mavi = '\033[94m'
+#Yeşil = '\033[92m'
+#Sarı = '\033[93m'
+#Pembe = '\33[95m'
+#Kırmızı = '\033[91m'
+#Kalın = '\033[1m'
+#AltıÇizili = '\033[4m'
+#Bitir = '\033[0m'
+#Beyaz ='\033[1;37m'
+#GriKarala = '\33[100m'
+#KırmızıKarala='\033[101m'
+#YeşilKarala='\33[102m'
+#SarıKarala='\33[103m'
+#MorKarala='\33[104m'
+#PembeKarala = '\33[105m'
+#CyanKarala = '\33[106m'
+#BeyazKarala = '\33[107m'
+print("""\033[92m
+ ██████╗ █████╗ ███╗   ███╗███████╗██████╗  █████╗       ███████╗███████╗███████╗
+██╔════╝██╔══██╗████╗ ████║██╔════╝██╔══██╗██╔══██╗      ██╔════╝██╔════╝██╔════╝
+██║     ███████║██╔████╔██║█████╗  ██████╔╝███████║█████╗███████╗█████╗  █████╗  
+██║     ██╔══██║██║╚██╔╝██║██╔══╝  ██╔══██╗██╔══██║╚════╝╚════██║██╔══╝  ██╔══╝  
+╚██████╗██║  ██║██║ ╚═╝ ██║███████╗██║  ██║██║  ██║      ███████║███████╗███████╗
+ ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝      ╚══════╝╚══════╝╚══════╝
+     Created By Wostarex
+       İstiklal Team  
+[Aze]-Camera
+[Tr]-Camera
+[Arm]-Camera
+
+           
+ 
+""")  
+camera = input("camera select:")
+if(camera == "Aze"):
+    print("""Kameralar Tapıldı!
+https://www.mylivestreams.com/azerbaijan-live-baku-city-youtube-video-cam-republic-of-azerbaijan/
+https://www.shahdag.az/en/live-cam
+https://www.webcamtaxi.com/en/azerbaijan.html
+http://www.insecam.org/en/bycountry/AZ/
+https://www.skylinewebcams.com/webcam/azerbaijan.html
+https://worldcam.eu/webcams/asia/azerbaijan/16590-baku-city-skyline  
+https://mobesekamerasi.com/azerbaycan-sahdag-kayak-merkezi-canli-izle/
+https://www.canlimobeseizle.com/azerbaycan-baku-canli-mobese-izle/  
+https://canlimobese.live/kebele-azerbaycan-kayak-merkezi-canli-izle/
+""")
+if(camera == "Tr"):
+    print("""Kameralar Tapıldı!
+https://mobesekamerasi.com/category/turkiye-canli-mobese/
+https://www.canlimobese.com/mobese/turkiye/
+https://www.canliseyir.com/category/turkiye-canli-kameralar/
+https://www.canlimobeseizle.com/mobese/turkiye/
+https://www.canlimobeseizle.com/sultanbeyli-canli-mobese-yayini-izle/
+http://www.kocbey.com/webcamturkiye.html
+https://istanbuluseyret.ibb.gov.tr/
+https://izum.izmir.bel.tr/apps/cameras/detail.html
+https://www.rize.bel.tr/hizmet/canli-kameralar
+https://www.konyabuyuksehir.tv/canliyayin_kameralar/turistik_kameralar/
+https://www.unye.bel.tr/sehir-rehberi/canli-kameralar/
+https://kocaeliyiseyret.com/
+https://www.tekirdag.bel.tr/sehir_kameralari
+https://www.bursa.bel.tr/sehir-kameralari
+https://www.uludaginfo.com/kameralar/uludag-canli-kameralar
+https://opmopmk.rihla.fi/
+""") 
+if(camera == "Arm"):
+    print("""
+https://www.canliseyir.com/category/ermenistan/
+https://www.canliseyir.com/tag/ermenistan-canli-izle/
+https://www.canlimobeseizle.com/uydu-goruntusu-haritasi/ermenistan/
+https://mobesekamerasi.com/category/kayak-merkezi-kameralari/ermenistan-kayak-merkezi/
+https://mobesekamerasi.com/ermenistan-tsaghkadzor-kayak-merkezi-canli-izle/
+""")    
